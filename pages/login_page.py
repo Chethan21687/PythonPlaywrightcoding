524176264b3ada72291276a5c38127768c71a642
Chethan21687 <168005023+Chethan21687@users.noreply.github.com>
2026-08-31 17:13:21 +0530

    Update Playwright script

--- pages/login_page.py
+++ pages/login_page.py
@@ -5,12 +5,12 @@ from pages.base_page import BasePage
 class LoginPage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
-        self.username_input = self.get_by_label('Username')
-        self.password_input = self.get_by_label('Password')
-        self.login_button = self.get_by_label('Login')
+        self.username_input = self.get_by_label("Username")
+        self.password_input = self.get_by_label("Password")
+        self.login_button = self.get_by_label("Login")
 
     def goto(self):
-        self._page.goto('/login')
+        self._page.goto("/login")
         self.wait_for_load()
 
     def login_with_valid_credentials(self, username, password):
@@ -18,5 +18,5 @@ class LoginPage(BasePage):
         self.password_input.fill(password)
         self.login_button.click()
         self.wait_for_load()
-        expect(self._page).to_have_url('/dashboard')
-        expect(self._page).to_have_title('Dashboard')
+        expect(self._page).to_have_url("/dashboard")
+        expect(self._page).to_have_title("Dashboard")
