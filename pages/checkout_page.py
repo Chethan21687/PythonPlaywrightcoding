c3e2d2eb6c34afbf1f24023c644e8f24a5dc41bd
Chethan21687 <168005023+Chethan21687@users.noreply.github.com>
2026-09-15 16:43:44 +0530

    Update Playwright script

--- pages/checkout_page.py
+++ pages/checkout_page.py
@@ -3,8 +3,9 @@ from playwright.sync_api import Page, expect
 from pages.base_page import BasePage
 
 class CheckoutPage(BasePage):
-    def __init__(self, page: Page):
-        super().__init__(page)
+    def __init__(self, page: Page) {
+        super().__init__(page);
+    }
 
     def goto(self):
         self._page.goto("/checkout")
